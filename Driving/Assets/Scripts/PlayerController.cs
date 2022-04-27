7a342a77bda024c2de74ea6ab06b47e127b2289d
ssuh0o0 <ssuh0o0@daum.net>
2022-04-27 21:01:36 +0900

    finish driving a car

--- Driving/Assets/Scripts/PlayerController.cs
+++ Driving/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward*Time.deltaTime*20);
+        // Get player Input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Vechicle forward
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*verticalInput);
         // transform.Translate(0,0,1);
+
+        // Vechicle turn y Axis 
+        transform.Rotate(Vector3.up ,Time.deltaTime*turnSpeed*horizontalInput);
     }
 }
