358fac90703ec842769b2e47161593dad248c593
ssuh0o0 <ssuh0o0@daum.net>
2022-05-02 00:26:20 +0900

    [2022/05/22] Player : Run Jump

--- Hurdle game/Assets/Scripts/PlayerController.cs
+++ Hurdle game/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)&&isOnGround)
+        {
+            playerRb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+
+        } else if (collision.gameObject.CompareTag("Obstacle")) {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
     }
 }
