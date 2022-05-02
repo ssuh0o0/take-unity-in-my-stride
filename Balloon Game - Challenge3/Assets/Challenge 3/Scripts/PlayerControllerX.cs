e1990c9d98736d0969d2b02341e0e3a8036db1be
ssuh0o0 <ssuh0o0@daum.net>
2022-05-02 20:47:34 +0900

    [2022/05/02] Chellenge 3 Game finished

--- Balloon Game - Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Balloon Game - Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -8,6 +8,8 @@ public class PlayerControllerX : MonoBehaviour
 
     public float floatForce;
     private float gravityModifier = 1.5f;
+    private float upperBound = 16;
+    private float lowerBound = 0 ;
     private Rigidbody playerRb;
 
     public ParticleSystem explosionParticle;
@@ -21,21 +23,32 @@ public class PlayerControllerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Physics.gravity *= gravityModifier;
-        playerAudio = GetComponent<AudioSource>();
 
-        // Apply a small upward force at the start of the game
-        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (transform.position.y > upperBound)
         {
-            playerRb.AddForce(Vector3.up * floatForce);
+            transform.position = new Vector3(transform.position.x, upperBound,transform.position.z);
+            playerRb.velocity = Vector3.zero;
+        }
+
+        if (transform.position.y < lowerBound)
+        {
+            transform.position = new Vector3(transform.position.x, lowerBound,transform.position.z);
+            playerRb.velocity = Vector3.zero;
+        }
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver )
+        {
+            playerRb.AddForce(Vector3.up * floatForce,ForceMode.Impulse);
         }
     }
 
