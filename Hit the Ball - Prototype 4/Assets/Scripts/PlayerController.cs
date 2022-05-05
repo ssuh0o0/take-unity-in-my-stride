4b5b5163223c9edd22aafaa34e085e281919e83e
ssuh0o0 <ssuh0o0@daum.net>
2022-05-05 23:25:08 +0900

    [2022/05/05] finished hit the ball game

--- Hit the Ball - Prototype 4/Assets/Scripts/PlayerController.cs
+++ Hit the Ball - Prototype 4/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {      
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public GameObject powerupIndicator;
+    private float powerupStrength = 15.0f;
     public float speed = 5.0f;
+    public bool hasPowerup = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +23,33 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        powerupIndicator.transform.position = transform.position + new Vector3(0f,-0.5f,0f);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRb.AddForce(awayFromPlayer *powerupStrength, ForceMode.Impulse);
+        }
     }
 }
--- Hit the Ball - Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Hit the Ball - Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    private float spawnRange = Random.Range(-9,9);
+    public GameObject powerupPrefab;
+    private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int enemyWave = 1;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(enemyWave);
+        SpawnPowerup();
+    }
+
+    void SpawnEnemyWave(int enemyToSpawn)
+    {
+        for (int i = 0 ; i < enemyToSpawn ; i++){
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnPowerup()
+    {
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            enemyWave++;
+            SpawnEnemyWave(enemyWave);
+            SpawnPowerup();
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
     {
-        float spawnPosX = spawnRange;
-        float spawnPosZ = spawnRange;
+        float spawnPosX = Random.Range(-spawnRange,spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange,spawnRange);
         Vector3 spawnPos = new Vector3(spawnPosX,0,spawnPosZ);
         return spawnPos;
     }
