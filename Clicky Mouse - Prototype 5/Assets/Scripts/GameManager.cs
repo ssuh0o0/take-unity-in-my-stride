3b4a5e1aca784d8ee59a615e360af78774cfb675
ssuh0o0 <ssuh0o0@daum.net>
2022-05-18 21:24:11 +0900

    [2022.05.18] Prototype 5 Game 5.3 finished

---
+++ Clicky Mouse - Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameoverText;
+
+    public Button restartButton;
+    private int score;
+
+    public bool isGameActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score\n   " + score;
+    }
+
+    public void GameOver()
+    {
+        gameoverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
