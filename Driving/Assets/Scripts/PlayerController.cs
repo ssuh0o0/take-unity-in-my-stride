252b45c9168670f27e4c96e959439dd3c9a36361
ssuh0o0 <ssuh0o0@daum.net>
2022-05-28 21:21:15 +0900

    [2022/05/28] Move car smoothly and Add speedText

--- Driving/Assets/Scripts/PlayerController.cs
+++ Driving/Assets/Scripts/PlayerController.cs
@@ -1,32 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-
-    private float speed = 10.0f;
-    private float turnSpeed = 25.0f;
+    [SerializeField] private float speed = 0;
+    [SerializeField] private float horsePower = 0;
+    private float turnSpeed = 50.0f;
     private float horizontalInput;
     private float verticalInput;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedometer;
+    void Start() {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
     }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Get player Input
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
         // Vechicle forward
-        transform.Translate(Vector3.forward*Time.deltaTime*speed*verticalInput);
+        // transform.Translate(Vector3.forward*Time.deltaTime*speed*verticalInput);
+        playerRb.AddRelativeForce(Vector3.forward* verticalInput*horsePower);
         // transform.Translate(0,0,1);
 
         // Vechicle turn y Axis 
         transform.Rotate(Vector3.up ,Time.deltaTime*turnSpeed*horizontalInput);
+
+        speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f); // For kph, change 2.237 -> 3.6
+        speedometer.SetText("Speed: "+speed+" m/h");
     }
 }
