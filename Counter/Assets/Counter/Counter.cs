87ae41d7afcb052a9fa4c0d10b120c9a80f5ee9a
ssuh0o0 <ssuh0o0@daum.net>
2022-06-01 18:33:52 +0900

    [2022/06/01] counter game

---
+++ Counter/Assets/Counter/Counter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Counter : MonoBehaviour
+{
+    public TextMeshProUGUI CounterText;
+
+    private int Count = 0;
+
+    private void Start()
+    {
+        Count = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Count += 1;
+        CounterText.text = "" + Count;
+    }
+}
